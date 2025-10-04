588a850d6eb082e7009c7dd601d0e94ff0d761bc
heygideon <47083178+heygideon@users.noreply.github.com>
2025-10-04 22:50:55 +0100

    feat: fix colour, add eyes and mouth

--- src/components/Meeple.vue
+++ src/components/Meeple.vue
@@ -1,16 +1,18 @@
 <script setup lang="ts">
-import { IconMoodSmile, IconPalette } from "@tabler/icons-vue";
-import meepleBlue from "~/assets/meeple-blue.png";
-import meepleCyan from "~/assets/meeple-cyan.png";
-import meepleOrange from "~/assets/meeple-orange.png";
-import meepleYellow from "~/assets/meeple-yellow.png";
+import { IconEye, IconMoodSmile, IconPalette } from "@tabler/icons-vue";
 import Tab from "./meeple/Tab.vue";
 
 import { provide, ref } from "vue";
+import { colours, eyes, mouths } from "~/lib/meeple";
 
-let tab = ref("colour");
-
+const tab = ref("colour");
 provide("tab", tab);
+
+const meeple = ref({
+  colour: "blue",
+  eyes: "",
+  mouth: "",
+});
 </script>
 
 <template>
@@ -18,27 +20,75 @@ provide("tab", tab);
     <div
       class="absolute inset-0 -z-10 rotate-2 rounded-3xl bg-neutral-50/50"
     ></div>
-    <div class="size-50">
-      <img :src="meepleBlue" alt="" class="size-full" />
-    </div>
+    <img
+      v-if="colours[meeple.colour]"
+      :src="colours[meeple.colour]?.src"
+      alt=""
+      class="size-full"
+    />
+    <img
+      v-if="eyes[meeple.eyes]"
+      :src="eyes[meeple.eyes]?.src"
+      alt=""
+      class="absolute top-16 left-19 h-3 w-auto translate-x-0.5"
+    />
+    <img
+      v-if="mouths[meeple.mouth]"
+      :src="mouths[meeple.mouth]?.src"
+      alt=""
+      class="absolute top-19 left-21 h-4 w-auto translate-x-0.5"
+    />
   </div>
 
   <div class="mt-8 rounded-xl bg-white p-1 shadow-md">
-    <div
-      class="flex gap-0.5 rounded-lg border-2 border-dashed border-neutral-200 p-6"
-    >
-      <Tab
-        currentValue="{tab}"
-        value="colour"
-        :icon="IconPalette"
-        label="Colour"
-      />
-      <Tab
-        currentValue="{tab}"
-        value="face"
-        :icon="IconMoodSmile"
-        label="Face"
-      />
+    <div class="rounded-lg border-2 border-dashed border-neutral-200 p-6">
+      <div class="flex gap-0.5">
+        <Tab value="colour" :icon="IconPalette" label="Colour" />
+        <Tab value="eyes" :icon="IconEye" label="Eyes" />
+        <Tab value="mouths" :icon="IconMoodSmile" label="Mouth" />
+      </div>
+
+      <div class="mt-4">
+        <div v-if="tab === 'colour'" class="flex flex-wrap gap-2">
+          <button
+            v-for="[key, colour] in Object.entries(colours)"
+            :key="key"
+            @click="meeple.colour = key"
+            :class="[
+              'size-14 flex-none rounded-md border border-neutral-200 bg-neutral-50',
+              meeple.colour === key && 'ring-2 ring-amber-600 ring-offset-1',
+            ]"
+          >
+            <img :src="colour.src" :alt="colour.label" class="size-full" />
+          </button>
+        </div>
+        <div v-if="tab === 'eyes'" class="flex flex-wrap gap-2">
+          <button
+            v-for="[key, eyes] in Object.entries(eyes)"
+            :key="key"
+            @click="meeple.eyes = key"
+            :class="[
+              'size-14 flex-none rounded-md border border-neutral-200 bg-neutral-50 px-2',
+              meeple.eyes === key && 'ring-2 ring-amber-600 ring-offset-1',
+            ]"
+          >
+            <img :src="eyes.src" :alt="eyes.label" class="h-auto w-full" />
+          </button>
+        </div>
+        <div v-if="tab === 'mouths'" class="flex flex-wrap gap-2">
+          <button
+            v-for="[key, mouth] in Object.entries(mouths)"
+            :key="key"
+            @click="meeple.mouth = key"
+            :class="[
+              'size-14 flex-none rounded-md border border-neutral-200 bg-neutral-50 px-2',
+              meeple.mouth === key && 'ring-2 ring-amber-600 ring-offset-1',
+            ]"
+          >
+            <img :src="mouth.src" :alt="mouth.label" class="h-auto w-full" />
+          </button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
--- src/lib/meeple.ts
+++ src/lib/meeple.ts
@@ -3,21 +3,161 @@ import meepleCyan from "~/assets/meeple-cyan.png";
 import meepleOrange from "~/assets/meeple-orange.png";
 import meepleYellow from "~/assets/meeple-yellow.png";
 
-export const colours = {
+import eyes1 from "~/assets/eyes/1.svg";
+import eyes2 from "~/assets/eyes/2.svg";
+import eyes3 from "~/assets/eyes/3.svg";
+import eyes4 from "~/assets/eyes/4.svg";
+import eyes5 from "~/assets/eyes/5.svg";
+import eyes6 from "~/assets/eyes/6.svg";
+import eyes7 from "~/assets/eyes/7.svg";
+import eyes8 from "~/assets/eyes/8.svg";
+import eyes9 from "~/assets/eyes/9.svg";
+import eyes10 from "~/assets/eyes/10.svg";
+import eyes11 from "~/assets/eyes/11.svg";
+import eyes12 from "~/assets/eyes/12.svg";
+import eyes13 from "~/assets/eyes/13.svg";
+import eyes14 from "~/assets/eyes/14.svg";
+import eyes15 from "~/assets/eyes/15.svg";
+import eyes16 from "~/assets/eyes/16.svg";
+import eyes17 from "~/assets/eyes/17.svg";
+import eyes18 from "~/assets/eyes/18.svg";
+import eyes19 from "~/assets/eyes/19.svg";
+import eyes20 from "~/assets/eyes/20.svg";
+import eyes21 from "~/assets/eyes/21.svg";
+import eyes22 from "~/assets/eyes/22.svg";
+import eyes23 from "~/assets/eyes/23.svg";
+import eyes24 from "~/assets/eyes/24.svg";
+import eyes25 from "~/assets/eyes/25.svg";
+import eyes26 from "~/assets/eyes/26.svg";
+import eyes27 from "~/assets/eyes/27.svg";
+import eyes28 from "~/assets/eyes/28.svg";
+import eyes29 from "~/assets/eyes/29.svg";
+import eyes30 from "~/assets/eyes/30.svg";
+import eyes31 from "~/assets/eyes/31.svg";
+
+import mouth1 from "~/assets/mouth/1.svg";
+import mouth2 from "~/assets/mouth/2.svg";
+import mouth3 from "~/assets/mouth/3.svg";
+import mouth4 from "~/assets/mouth/4.svg";
+import mouth5 from "~/assets/mouth/5.svg";
+import mouth6 from "~/assets/mouth/6.svg";
+import mouth7 from "~/assets/mouth/7.svg";
+import mouth8 from "~/assets/mouth/8.svg";
+import mouth9 from "~/assets/mouth/9.svg";
+import mouth10 from "~/assets/mouth/10.svg";
+import mouth11 from "~/assets/mouth/11.svg";
+import mouth12 from "~/assets/mouth/12.svg";
+import mouth13 from "~/assets/mouth/13.svg";
+import mouth14 from "~/assets/mouth/14.svg";
+import mouth15 from "~/assets/mouth/15.svg";
+import mouth16 from "~/assets/mouth/16.svg";
+import mouth17 from "~/assets/mouth/17.svg";
+import mouth18 from "~/assets/mouth/18.svg";
+import mouth19 from "~/assets/mouth/19.svg";
+import mouth20 from "~/assets/mouth/20.svg";
+import mouth21 from "~/assets/mouth/21.svg";
+import mouth22 from "~/assets/mouth/22.svg";
+import mouth23 from "~/assets/mouth/23.svg";
+import mouth24 from "~/assets/mouth/24.svg";
+import mouth25 from "~/assets/mouth/25.svg";
+import mouth26 from "~/assets/mouth/26.svg";
+import mouth27 from "~/assets/mouth/27.svg";
+import mouth28 from "~/assets/mouth/28.svg";
+import mouth29 from "~/assets/mouth/29.svg";
+import mouth30 from "~/assets/mouth/30.svg";
+
+type ColourMeta = {
+  label: string;
+  src: string;
+};
+
+export const colours: Record<string, ColourMeta> = {
   blue: {
     label: "Blue",
-    img: meepleBlue,
+    src: meepleBlue,
   },
   cyan: {
     label: "Cyan",
-    img: meepleCyan,
+    src: meepleCyan,
   },
   orange: {
     label: "Orange",
-    img: meepleOrange,
+    src: meepleOrange,
   },
   yellow: {
     label: "Yellow",
-    img: meepleYellow,
+    src: meepleYellow,
   },
+};
+
+type Item = {
+  label: string;
+  src: string;
+};
+
+export const eyes: Record<string, Item> = {
+  "1": { label: "1", src: eyes1 },
+  "2": { label: "2", src: eyes2 },
+  "3": { label: "3", src: eyes3 },
+  "4": { label: "4", src: eyes4 },
+  "5": { label: "5", src: eyes5 },
+  "6": { label: "6", src: eyes6 },
+  "7": { label: "7", src: eyes7 },
+  "8": { label: "8", src: eyes8 },
+  "9": { label: "9", src: eyes9 },
+  "10": { label: "10", src: eyes10 },
+  "11": { label: "11", src: eyes11 },
+  "12": { label: "12", src: eyes12 },
+  "13": { label: "13", src: eyes13 },
+  "14": { label: "14", src: eyes14 },
+  "15": { label: "15", src: eyes15 },
+  "16": { label: "16", src: eyes16 },
+  "17": { label: "17", src: eyes17 },
+  "18": { label: "18", src: eyes18 },
+  "19": { label: "19", src: eyes19 },
+  "20": { label: "20", src: eyes20 },
+  "21": { label: "21", src: eyes21 },
+  "22": { label: "22", src: eyes22 },
+  "23": { label: "23", src: eyes23 },
+  "24": { label: "24", src: eyes24 },
+  "25": { label: "25", src: eyes25 },
+  "26": { label: "26", src: eyes26 },
+  "27": { label: "27", src: eyes27 },
+  "28": { label: "28", src: eyes28 },
+  "29": { label: "29", src: eyes29 },
+  "30": { label: "30", src: eyes30 },
+  "31": { label: "31", src: eyes31 },
+};
+
+export const mouths: Record<string, Item> = {
+  "1": { label: "1", src: mouth1 },
+  "2": { label: "2", src: mouth2 },
+  "3": { label: "3", src: mouth3 },
+  "4": { label: "4", src: mouth4 },
+  "5": { label: "5", src: mouth5 },
+  "6": { label: "6", src: mouth6 },
+  "7": { label: "7", src: mouth7 },
+  "8": { label: "8", src: mouth8 },
+  "9": { label: "9", src: mouth9 },
+  "10": { label: "10", src: mouth10 },
+  "11": { label: "11", src: mouth11 },
+  "12": { label: "12", src: mouth12 },
+  "13": { label: "13", src: mouth13 },
+  "14": { label: "14", src: mouth14 },
+  "15": { label: "15", src: mouth15 },
+  "16": { label: "16", src: mouth16 },
+  "17": { label: "17", src: mouth17 },
+  "18": { label: "18", src: mouth18 },
+  "19": { label: "19", src: mouth19 },
+  "20": { label: "20", src: mouth20 },
+  "21": { label: "21", src: mouth21 },
+  "22": { label: "22", src: mouth22 },
+  "23": { label: "23", src: mouth23 },
+  "24": { label: "24", src: mouth24 },
+  "25": { label: "25", src: mouth25 },
+  "26": { label: "26", src: mouth26 },
+  "27": { label: "27", src: mouth27 },
+  "28": { label: "28", src: mouth28 },
+  "29": { label: "29", src: mouth29 },
+  "30": { label: "30", src: mouth30 },
 };
