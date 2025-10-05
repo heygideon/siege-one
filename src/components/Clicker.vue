4c5092271fac7aaa02d0a4d2b05339f6fbecded3
heygideon <47083178+heygideon@users.noreply.github.com>
2025-10-05 09:00:40 +0100

    feat: add animations to clicker

--- src/components/Clicker.vue
+++ src/components/Clicker.vue
@@ -3,18 +3,60 @@ import { inject, type Ref } from "vue";
 import coin from "~/assets/coin.png";
 
 const count = inject<Ref<number>>("count");
+
+const click = async (ev: MouseEvent) => {
+  if (!count) return;
+  count.value++;
+
+  const span = document.createElement("span");
+  span.textContent = "+1";
+  span.className = "button-click-span";
+  span.style.top = ev.clientY + "px";
+  span.style.left = ev.clientX + "px";
+  span.onanimationend = () => {
+    span.remove();
+  };
+  document.body.appendChild(span);
+};
 </script>
 
 <template>
   <div
     class="flex min-h-0 flex-1 flex-col items-center justify-center text-center"
   >
-    <button @click="count!++" class="group">
-      <img
-        :src="coin"
-        alt=""
-        class="h-24 drop-shadow-sm transition group-hover:scale-105 group-hover:drop-shadow-md"
-      />
-    </button>
+    <div>
+      <button
+        @click="click"
+        class="group grid size-32 place-items-center rounded-full"
+      >
+        <img
+          :src="coin"
+          alt=""
+          class="pointer-events-none w-32 drop-shadow-sm transition group-hover:scale-125 group-hover:drop-shadow-md group-active:scale-100"
+        />
+      </button>
+    </div>
   </div>
 </template>
+
+<style>
+@reference "../style.css";
+
+.button-click-span {
+  @apply pointer-events-none fixed text-lg font-semibold text-amber-700;
+  animation: button-click-span 0.5s ease-out forwards;
+}
+@keyframes button-click-span {
+  0% {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
+  25% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+    transform: translateY(-2rem) scale(1.5);
+  }
+}
+</style>
